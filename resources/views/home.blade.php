00a3b3568bb701898e3893dc418461457a33d4c2
toritakuro <toritakuro@gmail.com>
2022-07-07 21:02:52 +0900

    フォームから受け取った値をresultに送る

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,11 @@
                 <div class="card-header d-flex align-items-center justify-content-center">タンパク質の量を入力してください</div>
 
                 <div class="d-flex align-items-center justify-content-center m-4">
+                  <form action="/result" method="post">
+                    @csrf
                   {{Form::input('number','protein',0)}} g
                   <input type="submit" value="検索" >
+                </form>
                 </div>
             </div>
         </div>
