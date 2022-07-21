5859db0edbb4216ea6420feda0ffb966c53deda5
toritakuro <toritakuro@gmail.com>
2022-07-21 21:55:41 +0900

    タンパク質以外に脂質と炭水化物にも対応

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,28 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header d-flex align-items-center justify-content-center">タンパク質の量を入力してください</div>
+                <div class="card-header d-flex align-items-center justify-content-center">希望の栄養素の量を入力してください</div>
 
-                <div class="d-flex align-items-center justify-content-center m-4">
-                  <form action="/result" method="post">
+                <div class="d-flex  justify-content-center mt-1">
+                  <form class="p-5" action="/result" method="post">
                     @csrf
-                  {{Form::input('number','protein',0)}} g
-                  <input type="submit" value="検索" >
+                  タンパク質　
+                  <div class="col-xs-2">
+                    {{Form::input('number','protein')}} g
+                  </div>
+                  脂質　
+                  <div class="col-xs-2">
+                    {{Form::input('number','fat')}} g 以下
+                  </div>
+                  炭水化物
+                  <div class="col-xs-2">
+                    {{Form::input('number','carbo')}} g 以下
+                  </div>
+                  <input class="mt-2" type="submit" value="検索" >
                 </form>
                 </div>
+                <p class = "text-center">※タンパク質は入力された値の±５gで検索します</p>
+
             </div>
         </div>
     </div>
