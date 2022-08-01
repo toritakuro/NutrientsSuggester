375cccedab5475eaefda47267bb376614fc24fc4
toritakuro <toritakuro@gmail.com>
2022-08-01 20:44:40 +0900

    お気に入り削除後の画面を作成

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+     <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">{{ __('お気に入り商品') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="lh-lg">
+                      <?php
+
+                          echo $empty;
+
+                       ?>
+                <div class="d-flex justify-content-center p-3">
+                    <form class="text-center mr-2" action="/home" method="post">
+                        @csrf
+                        <input type="submit" value="ホームに戻る" >
+                    </form>
+                    <form class="text-center ml-2" action="/favorite" method="post">
+                        @csrf
+                        <input type="submit" value="一覧に戻る" >
+                    </form>
+                </div>
+
+
+
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
