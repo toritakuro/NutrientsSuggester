dab0ea8078fb1be692ae930e900779662e8bf88a
toritakuro <toritakuro@gmail.com>
2022-08-23 18:48:28 +0900

    https対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (App::environment(['production'])) {
+             URL::forceScheme('https');
+        }
+
     }
+
 }
